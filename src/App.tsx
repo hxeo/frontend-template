884cb9ba30c007f1d1473e5d1923a33791fee791
Aleksandar Simic <aleksandar.simic@rio.cloud>
2023-08-22 17:45:34 +0200

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 function App() {
-
   return (
     <div className={"h-screen bg-slate-200"}>
       <span className={"text-2xl bg"}>This is my begin!</span>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
